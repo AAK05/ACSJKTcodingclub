0c0d761a6ca88a8be42f8f2a33aa167112455bad
AAK05 <adrien.kusuma@gmail.com>
2021-09-21 07:42:56 +0700

    Added descriptions and instructions to each activity.

--- Python/activities/if_elif_else_activity.py
+++ Python/activities/if_elif_else_activity.py
@@ -1,3 +1,8 @@
+"""
+Activity: Write a script which asks for the user’s height in cm. 
+If the user’s height is less than 160, print “short”. 
+If user’s height is above 175, print “tall”. If user’s height is in between, print “average”.
+"""
 height = int(input("Please enter your height in cm"))
 #int() function changes datatype from string to integer, allowing mathematical comparisons
 if height < 160:
--- Python/activities/while_loop_activity.py
+++ Python/activities/while_loop_activity.py
@@ -1,3 +1,10 @@
+"""
+Activity: Write a script which asks for the user’s full name and gender. If gender is female, 
+ask for marital status of user. If user is male, print “Hello Mr {name}”. If user is female and married, 
+print “Hello Mrs {name}”. If user is female and unmarried, print “Hello Ms {name}”. 
+Make sure the script asks again for the gender or marital status if an unsuitable answer is provided 
+(e.g: the question is “Please enter your gender [M/F]”, and the answer was “123”, print(“invalid response”) before asking again)
+"""
 name = input("Please enter your full name.")
 gendervalid = False #variable which becomes True if a valid response for gender is recorded
 maritalvalid = False #variable which becomes True if a valid response for maritalstatus is recorded
