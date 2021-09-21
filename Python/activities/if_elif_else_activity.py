9cb0aad84a99e77741077cbfa6aa1f2f755e5f05
AAK05 <adrien.kusuma@gmail.com>
2021-09-21 17:14:47 +0700

    Modified if_elif_else_activity.py and used if_elif_else.py teaching

--- Python/activities/if_elif_else_activity.py
+++ Python/activities/if_elif_else_activity.py
@@ -1,13 +1,73 @@
 """
-Activity: Write a script which asks for the user’s height in cm. 
-If the user’s height is less than 160, print “short”. 
-If user’s height is above 175, print “tall”. If user’s height is in between, print “average”.
+Activity: Historical Figures Personality test
 """
-height = int(input("Please enter your height in cm"))
-#int() function changes datatype from string to integer, allowing mathematical comparisons
-if height < 160:
-    print("Short")
-elif height > 175:
-    print("Tall")
+Ada = 0
+Frieda = 0
+Lincoln = 0
+Moe = 0
+print("Which famous historical figure are you?")
+input("Press enter to begin the quiz")
+
+#Question1
+
+print("Would you rather:")
+print("a) Do something brand new")
+print("b) Improve something that already exists")
+qn1 = input()
+if qn1 == "a":
+    Ada = Ada + 1
+elif qn1 == "b":
+    Frieda = Frieda + 1
+    Lincoln = Lincoln + 1
 else:
-    print("Average")
+    print("Invalid answer")
+    Moe += 1
+
+#Question2
+
+print("My idea of fun is:")
+print("a) Spending time with friends")
+print("b) Creating art")
+print("c) Learning new things")
+qn2 = input()
+if qn2 == "a":
+    Lincoln += 1
+elif qn2 == "b":
+    Frieda += 1
+elif qn2 == "c":
+    Ada += 1
+else:
+    print("Invalid answer")
+    Moe += 1
+
+#Question3
+
+print("My favourite subject is:")
+print("a) English")
+print("b) Math")
+print("c) Art")
+qn3 = input()
+if qn3 == "a":
+    Lincoln += 1
+elif qn3 == "b":
+    Ada += 1
+elif qn3 == "c":
+    Frieda += 1
+else:
+    print("Invalid answer")
+    Moe += 1
+
+print("-- YOUR HISTORICAL FIGURE --")
+print("Abraham Lincoln:",Lincoln)
+print("Ada Lovelace:",Ada)
+print("Frieda Kahlo:",Frieda)
+print("-"*15)
+
+if Frieda > Lincoln and Frieda > Ada:
+    print("You are Frieda Kahlo")
+elif Lincoln > Frieda and Lincoln > Ada:
+    print("You are Abraham Lincold")
+elif Ada > Lincoln and Ada > Frieda:
+    print("You are Ada Lovelace")
+else:
+    print("Congrats wise guy, you're Moe Howard")
---
+++ Python/teaching/if_elif_else.py
@@ -0,0 +1,7 @@
+x = int(input("Define a numerical value for x"))
+if x > 5:
+    print("x is larger than 5")
+elif x < 5:
+    print("x is smaller than 5")
+else:
+    print("x is equal to 5")
